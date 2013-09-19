879c5c59217d6f907be383e2ce6a461780da1af7
Junior Ales <edilson.ales.jr@gmail.com>
2013-09-19 00:24:14 -0300

    Pagina temporaria

--- app.rb
+++ app.rb
@@ -4,11 +4,11 @@ require 'csv'
 get '/' do
   @link_localizacao = "https://www.google.com/maps/preview#!data=!1m4!1m3!1d3055!2d-51.1524983!3d-30.0032815!4m25!2m11!1m10!1s0x0%3A0xe645f0d5030c43ec!3m8!1m3!1d26081603!2d-95.677068!3d37.0625!3m2!1i1024!2i768!4f13.1!5m12!1m11!1slindoia+tenis+clube+poa!4m8!1m3!1d26081603!2d-95.677068!3d37.0625!3m2!1i1024!2i768!4f13.1!17b1"
 
-   erb :index
+   erb :construcao
 end
 
-get '/cartaz' do
-  erb :cartaz
+get '/dev' do
+  erb :index
 end
 
 get '/cronograma' do
