66eb0557123bdc57ab53a8646b4460c4dd6363ed
Junior Ales <edilson.ales.jr@gmail.com>
2013-09-19 01:13:53 -0300

    Refatorando e fazendo botoes da tela inicial funcionar

--- public/js/sakuracup.js
+++ public/js/sakuracup.js
@@ -1,6 +1,6 @@
 var todosInscritos = function() {
-  var linhas = $(".tabela").find("tr");
-  return $("tbody").find(linhas);
+  var linhas = $('.tabela').find('tr');
+  return $('tbody').find(linhas);
 }
 
 var escondeTodosInscritos = function() {
@@ -12,29 +12,35 @@ var mostraTodosInscritos = function() {
 }
 
 var mostraSomenteInscritosFaixa = function(corDaFaixa) {
-  $(".tabela").find(".faixa-" + corDaFaixa).fadeIn();
+  $('.tabela').find('.faixa-' + corDaFaixa).fadeIn();
 }
 
 var desabilitaTodosFiltros = function() {
-  $(".filtro-por-faixa").find("img").removeClass('habilitado');
+  $('.filtro-por-faixa').find('img').removeClass('habilitado');
+}
+
+var filtrarPorFaixa = function(event) {
+  event.preventDefault();
+
+  var filtro = $(this).find('img');
+  desabilitaTodosFiltros();
+  filtro.addClass('habilitado');
+
+  escondeTodosInscritos();
+
+  var faixa = filtro.data('faixa');
+
+  if (faixa === 'todas') {
+    mostraTodosInscritos();
+  } else { 
+    mostraSomenteInscritosFaixa(faixa);
+  }
 }
 
 $(document).ready(function() {
-  $(".filtro-por-faixa").on('click', function(event) {
-    event.preventDefault();
+  $('.filtro-por-faixa').on('click', filtrarPorFaixa);
 
-    var filtro = $(this).find("img");
-    desabilitaTodosFiltros();
-    filtro.addClass('habilitado');
-
-    escondeTodosInscritos();
-
-    var faixa = filtro.data("faixa");
-
-    if (faixa === "todas") {
-      mostraTodosInscritos();
-    } else { 
-      mostraSomenteInscritosFaixa(faixa);
-    }
+  $('.sub-paineis').on('click', 'button', function() {
+    $(this).closest('section').find('a')[0].click();
   });
 });
