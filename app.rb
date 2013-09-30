11844eb746b22f4930eac94833ed918db96139af
Junior Ales <edilson.ales.jr@gmail.com>
2013-09-30 19:57:51 -0300

    Pagina temporaria do SCIII criada

--- app.rb
+++ app.rb
@@ -2,26 +2,27 @@ require 'sinatra'
 require 'csv'
 
 get '/' do
-  erb :index
+  erb :construcao
+  #erb :index
 end
 
-get '/cronograma' do
-  erb :cronograma
-end
-
-get '/informacoes' do
-  erb :informacoes
-end
-
-get '/inscritos' do
-  @helper = Helper.new
-  erb :inscritos
-end
-
-get '/fotos' do
-  erb :fotos
-end
-
-get '/premiacao' do
-  erb :premiacao
-end
+#get '/cronograma' do
+#  erb :cronograma
+#end
+#
+#get '/informacoes' do
+#  erb :informacoes
+#end
+#
+#get '/inscritos' do
+#  @helper = Helper.new
+#  erb :inscritos
+#end
+#
+#get '/fotos' do
+#  erb :fotos
+#end
+#
+#get '/premiacao' do
+#  erb :premiacao
+#end
